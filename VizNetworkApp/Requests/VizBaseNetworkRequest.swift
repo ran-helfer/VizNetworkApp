0a0e1e9dffe8c8b8847ec1d76a7519347c6f8433
Ran Helfer <ran@viz.ai>
2021-07-29 09:28:59 +0300

    reduce code duplications

--- VizNetworkApp/Requests/VizBaseNetworkRequest.swift
+++ VizNetworkApp/Requests/VizBaseNetworkRequest.swift
@@ -14,55 +14,51 @@ protocol VizBaseNetworkRequest: AnyObject {
 }
 
 extension VizBaseNetworkRequest {
-    func load(_ request: URLRequest, withCompletion completion: @escaping (Result<ModelType, Error>) -> Void) {
+    func load(_ request: URLRequest,
+              withCompletion completion: @escaping (Result<ModelType, Error>) -> Void) {
         let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            guard error == nil else {
-                if let err = error {
-                    completion(.failure(err))
-                } else {
-                    completion(.failure(VizBaseNetworkRequestError.recievedErrorFromServer))
-                }
-                return
-            }
-            
-            guard let weakSelf = self,
-                  let data = data,
-                  let value =  weakSelf.decodeData(data) else {
-                DispatchQueue.main.async {
-                    completion(.failure(VizBaseNetworkRequestError.failed))
-                }
-                return
-            }
-            DispatchQueue.main.async {
-                completion(.success(value))
-            }
+            guard let weakSelf = self else {return}
+            weakSelf.handleRequestResponse(data: data,
+                                           response: response,
+                                           error: error,
+                                           completion: completion)
         }
         task.resume()
     }
     
     func load(_ url: URL, withCompletion completion: @escaping (Result<ModelType, Error>) -> Void) {
         let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response , error) -> Void in
-            guard error == nil else {
-                if let err = error {
-                    completion(.failure(err))
-                } else {
-                    completion(.failure(VizBaseNetworkRequestError.recievedErrorFromServer))
-                }
-                return
-            }
-            
-            print(response as Any)
-            guard let weakSelf = self,
-                  let data = data,
-                  let value =  weakSelf.decodeData(data) else {
-                DispatchQueue.main.async {
-                    completion(.failure(VizBaseNetworkRequestError.failed))
-                }
-                return
-            }
-            DispatchQueue.main.async { completion(.success(value)) }
+            guard let weakSelf = self else {return}
+            weakSelf.handleRequestResponse(data: data,
+                                           response: response,
+                                           error: error,
+                                           completion: completion)
         }
         task.resume()
+    }
+    
+    private func handleRequestResponse(data:Data?,
+                                       response:Any?,
+                                       error:Error?,
+                                       completion: @escaping (Result<ModelType, Error>) -> Void) {
+        guard error == nil else {
+            if let err = error {
+                completion(.failure(err))
+            } else {
+                completion(.failure(VizBaseNetworkRequestError.recievedErrorFromServer))
+            }
+            return
+        }
+        
+        print(response as Any)
+        guard let data = data,
+              let value =  self.decodeData(data) else {
+            DispatchQueue.main.async {
+                completion(.failure(VizBaseNetworkRequestError.failed))
+            }
+            return
+        }
+        DispatchQueue.main.async { completion(.success(value))}
     }
 }
 
