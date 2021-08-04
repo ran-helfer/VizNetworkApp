732935c1027597f10e3b8d9fe37c0d63ab7e3cda
Ran Helfer <ran@viz.ai>
2021-08-04 17:40:04 +0300

    Adding requests file and removing redundant API from end_points

---
+++ VizNetworkApp/Requests.swift
@@ -0,0 +1,95 @@
+//
+//  Requests.swift
+//  VizNetworkApp
+//
+//  Created by Ran Helfer on 04/08/2021.
+//
+
+import Foundation
+
+/***********/
+/**  GET  **/
+/***********/
+
+struct RemoteGetResource: VizHttpApiResource {
+    var headers: [String : String]?
+    
+    var queryItems: [URLQueryItem]?
+    
+    
+    /* at terminal run
+        python api_endpoints.py
+    */
+    
+    typealias ModelType = UsersList
+
+    var basePath: String {
+        "http://127.0.0.1:5000"
+    }
+    
+    var path: String {
+        "/users"
+    }
+    
+    var method: VizHttpMethod {
+        .get(nil)
+    }
+}
+
+/***********/
+/**  POST **/
+/***********/
+
+struct RemotePostResource: VizHttpApiResource {
+    var headers: [String : String]? =
+        ["Content-Type" : "application/json"]
+    
+    var queryItems: [URLQueryItem]?
+    
+    var method: VizHttpMethod
+    
+    
+    /* at terminal run
+        python api_endpoints.py
+    */
+    
+    typealias ModelType = UserObject
+
+    var basePath: String {
+        "http://127.0.0.1:5000"
+    }
+    
+    var path: String {
+        "/users"
+    }
+}
+
+/*************/
+/**  DELETE **/
+/*************/
+
+
+
+struct RemoteDeleteResource: VizHttpApiResource {
+    var headers: [String : String]? =
+        ["Content-Type" : "application/json"]
+    
+    var queryItems: [URLQueryItem]?
+    
+    var method: VizHttpMethod
+    
+    
+    /* at terminal run
+        python api_endpoints.py
+    */
+    
+    typealias ModelType = UserObject
+
+    var basePath: String {
+        "http://127.0.0.1:5000"
+    }
+    
+    var path: String {
+        "/users"
+    }
+}
